ce6d773a6f2edaee1c18a1cc99a97f8a523139f3
Daniel Le Petit <dlepetit@hotmail.fr>
2025-09-20 12:15:07 +0200

    Fix TypeScript build - working locally

---
+++ deployLT.ps1
@@ -0,0 +1,3 @@
+C:\"Program files"\git\bin\git.exe remote -v
+C:\"Program files"\git\bin\git.exe remote set-url origin https://github.com/Daniel-Le-Petit/PersonalLeaveTracker.git
+C:\"Program files"\git\bin\git.exe push --force origin main
